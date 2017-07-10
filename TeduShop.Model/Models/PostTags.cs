4a1e5f5be8a7bc4169be90360180e2b4890e05d8
LAPTOP-NHANNV\Luong Thuy Duong <nvnhan2004@gmail.com>
2017-07-10 15:56:59 +0700

    add model table

---
+++ TeduShop.Model/Models/PostTags.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeduShop.Model.Models
+{
+    [Table("PostTags")]
+    public class PostTags
+    {
+        [Key]
+        public int PostID { set; get; }
+
+        [Key]
+        [MaxLength(50)]
+        [Column(TypeName = "varchar")]
+        public string TagID { set; get; }
+
+        [ForeignKey("TagID")]
+        public virtual Tag Tag { set; get; }
+
+        [ForeignKey("PostID")]
+        public virtual Post Post { set; get; }
+    }
+}
---
+++ TeduShop.Model/Models/Tags.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeduShop.Model.Models
+{
+    [Table("Tags")]
+    public class Tags
+    {
+        [Key]
+        [MaxLength(50)]
+        public int ID { set; get; }
+
+        [Required]
+        [MaxLength(256)]
+        public string Name { set; get; }
+
+        [Required]
+        [MaxLength(256)]
+        public string Type { set; get; }
+    }
+}
